1ffa015de6f3071b5097fb22ec6441161941f6c0
krishna jain <krishnajain5050@gmail.com>
2024-06-04 12:08:13 +0530

    user registration setup

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,29 @@
+# Define the Node.js version as a build argument
 ARG NODE_VERSION=21.2.0
 
+# Use an official Node runtime as a parent image
 FROM node:${NODE_VERSION}-alpine
 
+# Set the NODE_ENV environment variable
 ENV NODE_ENV production
 
+# Set the working directory in the container
 WORKDIR /usr/src/app
 
-RUN --mount=type=bind,source=package.json,target=package.json \
-    --mount=type=bind,source=package-lock.json,target=package-lock.json \
-    --mount=type=cache,target=/root/.npm \
-    npm ci --omit=dev
+# Copy the package.json and package-lock.json files to the working directory
+COPY package.json package-lock.json ./
 
+# Install all dependencies, including devDependencies
+RUN npm install
+
+# Switch to a non-root user
 USER node
 
+# Copy the rest of the application code to the working directory
 COPY . .
 
+# Expose the port the app runs on
 EXPOSE 3000
 
-CMD npm run dev
+# Define the command to run the app
+CMD ["npm", "run", "dev"]
--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,6 +2,9 @@
 
 import { z } from "zod"
 import { RegisterSchema } from '@/schemas'
+import bcrypt from 'bcrypt'
+import {db} from '@/lib/db'
+import { getUserByEmail } from "@/data/user"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
@@ -10,6 +13,27 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
             error: "Invalid Fields!!"
         }
     }
+
+    const {email, password, name} = validatedFields.data
+    const pwHash = await bcrypt.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email)
+
+    if (existingUser) {
+        return {
+            error: "User Already Exists!!"
+        }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: pwHash
+        }
+    })
+
+    //TODO Send verification token message
 
     return {
         success: "Email Sent!!"
